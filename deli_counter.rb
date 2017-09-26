4ed195f2eba15ceaac3650dae271294de6b74f01
John Tuttle <thejwt@gmail.com>
2017-09-26 01:28:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number (deli_array, name)
     deli_array.push(name)
     puts "Welcome, #{name}. You are number #{deli_array.length} in line."
 end
+
+def now_serving (deli_array)
+  if deli_array == 0
+    puts "The line is currently empty."
+  else
+    puts "now serving #{deli_array[0]}."
+    deli_array.shift
+  end
+end
